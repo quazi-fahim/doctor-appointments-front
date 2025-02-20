622b605019740f4b0dcc93f92c626b7978a5a895
Quazi fahim <fahimahmad.4388@gmail.com>
2025-02-20 20:50:06 +0530

    FIx for prod

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-  host: true,
-  strictPort: true,
-  port: 3000,
+    host: true,
+    strictPort: true,
+    port: 3000,
+    allowedHosts: ['doctor-appointments-front.onrender.com'],
   }
 })
